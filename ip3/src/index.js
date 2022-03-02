6e32d18a0e5472adbb62f24da64421ca7ce8ce41
“jeffe419” <jeffersonatigah2000@gmail.com>
2022-03-02 04:15:34 +0000

    Nav bar test

--- ip3/src/index.js
+++ ip3/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import ReactDOm from 'react-dom';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const element = <>Hello World</>;
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOm.render(element, document.getElementById('root')); 
+
+ReactDOM.render(<App />, document.getElementById('root') );
+  
+
