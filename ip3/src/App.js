1436899a879e5d54648cb935bbefb3ffab2608f3
“jeffe419” <jeffersonatigah2000@gmail.com>
2022-03-02 04:20:02 +0000

    Prototype Nav bar

--- ip3/src/App.js
+++ ip3/src/App.js
@@ -7,24 +7,17 @@ function App(){
         <div className="App">
         <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
          <ReactBootStrap.Container>
-        <ReactBootStrap.Navbar.Brand href="#home">React-Bootstrap</ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Brand href="#home">Save The Polar Bears</ReactBootStrap.Navbar.Brand>
   <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
     <ReactBootStrap.Nav className="me-auto">
-      <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Divider />
-        <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
-      </ReactBootStrap.NavDropdown>
+      <ReactBootStrap.Nav.Link href="#features">Weather History Data</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#pricing">CO2 Data</ReactBootStrap.Nav.Link>
     </ReactBootStrap.Nav>
     <ReactBootStrap.Nav>
-      <ReactBootStrap.Nav.Link href="#deets">More deets</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#deets">News</ReactBootStrap.Nav.Link>
       <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
-        Dank memes
+        About
       </ReactBootStrap.Nav.Link>
     </ReactBootStrap.Nav>
   </ReactBootStrap.Navbar.Collapse>
