e0dec58b763cc0d48b48d4125a17e9894a86c01b
jeffe419 <jeffersonatigah2000@gmail.com>
2022-03-18 00:33:44 +0000

    implementing External javascript

--- ip3/src/components/weatherhData.jsx
+++ ip3/src/components/weatherhData.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 import * as ReactBootStrap from "react-bootstrap";
+import './weather.js';
 
-function WeatherhData(){
-    return(
-    <h1>WeatherData</h1>
-    );
+function WeatherhData() {
+  return (
+<div id="map">
+  <p id="demo">A Paragraph.</p>
+  <button type="button" onclick="myFunction()">
+    Try it
+  </button>
+  <p>This example links to "myScript.js".</p>
+  <p>(myFunction is stored in "myScript.js")</p>
+  <script src="weather.js"></script>
+</div>
+
+
+  );
 }
 
 export default WeatherhData;
